d5e1f58dfe04b47dae6db72e8a90dbad7f6eb28d
hyeonic <dev.hyeonic@gmail.com>
2022-09-17 01:42:57 +0900

    test: Junit4TestRunner 실습 추가

--- reflection/src/test/java/reflection/Junit4TestRunner.java
+++ reflection/src/test/java/reflection/Junit4TestRunner.java
@@ -1,5 +1,8 @@
 package reflection;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Arrays;
 import org.junit.jupiter.api.Test;
 
 class Junit4TestRunner {
@@ -7,7 +10,25 @@ class Junit4TestRunner {
     @Test
     void run() throws Exception {
         Class<Junit4Test> clazz = Junit4Test.class;
+        Junit4Test instance = clazz.getDeclaredConstructor()
+                .newInstance();
 
         // TODO Junit4Test에서 @MyTest 애노테이션이 있는 메소드 실행
+        Method[] methods = clazz.getDeclaredMethods();
+        Arrays.stream(methods)
+                .filter(this::containsAnnotation)
+                .forEach(method -> invoke(instance, method));
+    }
+
+    private boolean containsAnnotation(final Method method) {
+        return method.isAnnotationPresent(MyTest.class);
+    }
+
+    private Object invoke(final Junit4Test instance, final Method method) {
+        try {
+            return method.invoke(instance);
+        } catch (IllegalAccessException | InvocationTargetException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
