e6be8204dc3a64833fb8a1cc9f172ff48d2072ab
hyeonic <dev.hyeonic@gmail.com>
2022-09-30 12:31:11 +0900

    feat: 4단계 실습 완료

--- di/src/test/java/nextstep/study/di/stage4/annotations/ClassPathScanner.java
+++ di/src/test/java/nextstep/study/di/stage4/annotations/ClassPathScanner.java
@@ -1,10 +1,20 @@
 package nextstep.study.di.stage4.annotations;
 
+import java.lang.annotation.Annotation;
+import java.util.Collection;
 import java.util.Set;
+import java.util.stream.Collectors;
+import org.reflections.Reflections;
 
 public class ClassPathScanner {
 
+    private static final Set<Class<? extends Annotation>> ANNOTATION_CLASSES = Set.of(Repository.class, Service.class);
+
     public static Set<Class<?>> getAllClassesInPackage(final String packageName) {
-        return null;
+        Reflections reflections = new Reflections(packageName);
+        return ANNOTATION_CLASSES.stream()
+                .map(reflections::getTypesAnnotatedWith)
+                .flatMap(Collection::stream)
+                .collect(Collectors.toSet());
     }
 }
--- di/src/test/java/nextstep/study/di/stage4/annotations/DIContainer.java
+++ di/src/test/java/nextstep/study/di/stage4/annotations/DIContainer.java
@@ -1,6 +1,9 @@
 package nextstep.study.di.stage4.annotations;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * 스프링의 BeanFactory, ApplicationContext에 해당되는 클래스
@@ -10,15 +13,61 @@ class DIContainer {
     private final Set<Object> beans;
 
     public DIContainer(final Set<Class<?>> classes) {
-        this.beans = Set.of();
+        this.beans = toInstances(classes);
+        for (Object bean : beans) {
+            injectPossibleFields(bean);
+        }
+    }
+
+    private void injectPossibleFields(final Object bean) {
+        Class<?> beanClass = bean.getClass();
+        for (Field field : beanClass.getDeclaredFields()) {
+            inject(bean, field);
+        }
+    }
+
+    private void inject(final Object bean, final Field field) {
+        if (field.isAnnotationPresent(Inject.class)) {
+            setField(bean, field);
+        }
+    }
+
+    private void setField(final Object bean, final Field field) {
+        try {
+            Object fieldValue = getBean(field.getType());
+            field.setAccessible(true);
+            field.set(bean, fieldValue);
+            field.setAccessible(false);
+        } catch (final IllegalAccessException e) {
+            throw new RuntimeException("존재하지 않는 field입니다.");
+        }
+    }
+
+    private Set<Object> toInstances(final Set<Class<?>> classes) {
+        return classes.stream()
+                .map(this::toInstance)
+                .collect(Collectors.toSet());
+    }
+
+    private Object toInstance(final Class<?> aClass) {
+        try {
+            Constructor<?> constructor = aClass.getDeclaredConstructor();
+            constructor.setAccessible(true);
+            return constructor.newInstance();
+        } catch (final Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public static DIContainer createContainerForPackage(final String rootPackageName) {
-        return null;
+        return new DIContainer(ClassPathScanner.getAllClassesInPackage(rootPackageName));
     }
 
     @SuppressWarnings("unchecked")
     public <T> T getBean(final Class<T> aClass) {
-        return null;
+        return (T) beans.stream()
+                .filter(bean -> aClass.isAssignableFrom(bean.getClass()))
+                .findFirst()
+                .orElse(null);
     }
 }
